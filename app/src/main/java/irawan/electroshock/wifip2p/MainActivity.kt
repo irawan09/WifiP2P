bc4c1caacef393136e32dfea356c7cdf1245da3f
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-04-04 18:56:18 +0200

    Update peering device

--- app/src/main/java/irawan/electroshock/wifip2p/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/wifip2p/MainActivity.kt
@@ -1,25 +1,36 @@
 package irawan.electroshock.wifip2p
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pDeviceList
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import android.provider.Settings
-import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import irawan.electroshock.wifip2p.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var wifiManager: WifiManager
-    private lateinit var mManager: WifiP2pManager
+    private val mManager: WifiP2pManager by lazy (LazyThreadSafetyMode.NONE){
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+    }
     private lateinit var mChannel: WifiP2pManager.Channel
     private lateinit var mReceiver: BroadcastReceiver
     private lateinit var mIntentFilter: IntentFilter
+    var peers : MutableList<WifiP2pDevice> = mutableListOf<WifiP2pDevice>()
+    lateinit var deviceNameArray : Array<String?>
+    lateinit var deviceArray : Array<WifiP2pDevice?>
     val TAG = "STATUS"
 
 
@@ -30,16 +41,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        mManager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
-        mChannel = mManager.initialize(this, Looper.getMainLooper(), null)
-
-        mReceiver = WifiDirectBroadcastReceiver(mManager, mChannel, this)
-        mIntentFilter = IntentFilter()
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
-
+        mChannel = mManager.initialize(this, mainLooper, null)!!
+        mChannel.also { channel ->
+            mReceiver = WifiDirectBroadcastReceiver(mManager, channel, this)
+        }
+        mIntentFilter = IntentFilter().apply {
+            addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+        }
 
         binding.onOff.setOnClickListener {
             if (wifiManager.isWifiEnabled) {
@@ -62,13 +73,100 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.discover.setOnClickListener {
+            val hasWritePermission:Int = ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            if (hasWritePermission != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION)){
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        1)
+                } else{
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        1)
+                }
 
+            }
+            mManager.discoverPeers(mChannel, object : WifiP2pManager.ActionListener{
+                override fun onSuccess() {
+                    binding.connectionStatus.text = getString(R.string.discovery)
+                }
+
+                override fun onFailure(p0: Int) {
+                    binding.connectionStatus.text = getString(R.string.discovery_failed)
+                }
+
+            })
         }
 
         binding.sendButton.setOnClickListener {
 
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION) ==
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(
+                            this,
+                            "Permission Granted",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Permission Denied",
+                        Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    val peerListListener : WifiP2pManager.PeerListListener = object : WifiP2pManager.PeerListListener {
+        override fun onPeersAvailable(peerList: WifiP2pDeviceList?) {
+            if(!peerList?.deviceList?.equals(peers)!!){
+                peers.clear()
+                peers.addAll(peerList.deviceList)
+
+                deviceNameArray = arrayOfNulls<String>(peerList.deviceList.size)
+                deviceArray = arrayOfNulls<WifiP2pDevice>(peerList.deviceList.size)
+                var index:Int = 0
+                peerList.deviceList.forEach { device ->
+                    deviceNameArray[index] = device.deviceName
+                    deviceArray[index] = device
+                    index++
+                }
+
+                val deviceNameAdapter: ArrayAdapter<String> = ArrayAdapter(
+                    applicationContext,
+                android.R.layout.simple_list_item_1,
+                deviceNameArray)
+                binding.peerListView.adapter = deviceNameAdapter
+            }
+
+            if (peers.size == 0){
+                Toast.makeText(applicationContext,"No Device Found", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
 
     override fun onResume() {
         super.onResume()
@@ -79,4 +177,6 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         unregisterReceiver(mReceiver)
     }
+
+
 }
--- app/src/main/java/irawan/electroshock/wifip2p/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/irawan/electroshock/wifip2p/WifiDirectBroadcastReceiver.kt
@@ -1,11 +1,15 @@
 package irawan.electroshock.wifip2p
 
+import android.Manifest
 import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class WifiDirectBroadcastReceiver(
     private val mManager: WifiP2pManager,
@@ -23,11 +27,59 @@ class WifiDirectBroadcastReceiver(
                 Toast.makeText(context, "WiFi is OFF", Toast.LENGTH_LONG).show()
             }
         } else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
-            //do something
+            val hasWritePermission:Int = ActivityCompat.checkSelfPermission(
+                mActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            if (hasWritePermission != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        mActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION)){
+                    ActivityCompat.requestPermissions(
+                        mActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        1)
+                } else{
+                    ActivityCompat.requestPermissions(
+                        mActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        1)
+                }
+
+            }
+            mManager.requestPeers(mChannel, mActivity.peerListListener)
         } else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
             //do something
         } else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
             // do something
         }
     }
+
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(mActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION) ==
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(
+                            mActivity,
+                            "Permission Granted",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(
+                        mActivity,
+                        "Permission Denied",
+                        Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
 }
